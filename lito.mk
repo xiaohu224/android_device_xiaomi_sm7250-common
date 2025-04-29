0479da1cd7fa486dfc4503669a7c8626091e403b
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    sm7250-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- lito.mk
+++ lito.mk
@@ -149,7 +149,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libstdc++_vendor
-    
+
 $(call soong_config_set,camera,override_format_from_reserved,true)
 
 # Camera Legacy HIDL deps
@@ -312,6 +312,8 @@ PRODUCT_PACKAGES += \
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
+
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 
 # Logging
 SPAMMY_LOG_TAGS := \
