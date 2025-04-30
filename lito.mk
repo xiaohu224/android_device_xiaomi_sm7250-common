81728f23760998451acf877de3457160e3d1da11
Bruno Martins <bgcngm@gmail.com>
2025-04-30 21:12:33 +0100

    sm7250-common: Set camera override format from reserved via soong config
    Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550

--- lito.mk
+++ lito.mk
@@ -149,6 +149,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libstdc++_vendor
+    
+$(call soong_config_set,camera,override_format_from_reserved,true)
 
 # Camera Legacy HIDL deps
 PRODUCT_PACKAGES += \
