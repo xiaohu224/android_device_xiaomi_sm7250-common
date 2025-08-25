6ebc26c8cd2a46cc1541aff0957e7511d77bab9d
LuK1337 <priv.luk@gmail.com>
2025-08-26 00:38:01 +0530

    sm7250-common: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- lito.mk
+++ lito.mk
@@ -312,7 +312,9 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay-service.sdm
+
+$(call soong_config_set_bool,livedisplay_sdm,enable_dm,false)
 
 # Lineage Health
 PRODUCT_PACKAGES += \
